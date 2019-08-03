7bbdb80d6a67c1ea91072ef9223a397993221ce7
lukas simianer <lsimianer@gmail.com>
2019-08-03 11:20:04 -0500

    pushing for individual deployment. styling is done. db functional. save function and all buttons working. 404 page working.

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../List/List.css";
+
 
 export const FormBtn = props => (
   <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-info">
